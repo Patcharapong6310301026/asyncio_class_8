a6c1cf99543052e71144c086e8de4e7bc0b548c6
Patcharapong6310301026 <71427949+Patcharapong6310301026@users.noreply.github.com>
2023-08-29 19:51:01 +0700

    Add files via upload

---
+++ 3-queue-waitfor.py
@@ -0,0 +1,50 @@
+from random import random
+import asyncio
+import time
+
+# coroutine to generate work
+async def producer(queue):
+    print('Producer: Running')
+    # generate work
+    for i in range(10):
+        # generate a value
+        value = random ()
+        # block to simulate work
+        await asyncio.sleep(value)
+        # add to the queue
+        await queue.put(value)
+    #send an all done signal
+    await queue.put(None)
+    print(f'{time.ctime()} Producer: Done')
+    
+# consume work
+async def consumer(queue):
+    print(f'{time.ctime()} Consumer: Running')
+    #consume work
+    while True:
+        # get a unit of work
+        try:
+            # retrieve the get() awaitable
+            get_await = queue.get()
+            #await the awaitable with a timeout
+            item = await asyncio.wait_for(get_await, 0.5)
+        except asyncio.TimeoutError:
+            print(f'{time.ctime()} Consumer: gave up waiting...')
+            continue
+        # check for stop
+        if item is None:
+            break
+        #report
+        print(f'{time.ctime()} >got {item}')
+    #all done
+    print('Consumer: Done')
+    
+# entry point coroutine
+async def main():
+    # create the shared queue
+    queue = asyncio.Queue()
+    # run the producer and consumers
+    await asyncio.gather(producer(queue), consumer(queue))
+    
+# start the asyncio program
+asyncio.run(main())
